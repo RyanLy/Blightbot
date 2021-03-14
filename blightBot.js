298e2267add6bd7af73b1512c822a49f18aa3947
Ryan <windask@hotmail.com>
2021-03-14 07:23:42 -0700

    Even smarter AI

--- blightBot.js
+++ blightBot.js
@@ -49,13 +49,16 @@ function findPlaceWithUnclaimedUnit(militiaKind = 'human_standard') {
 
 function findEnemyUnits() {
   return galaxy.unitList
-    .filter(unit => unit.undead === 1)
-    .sort((unit1, unit2) => getArmyMight(unit1) - getArmyMight(unit2))
-    .reverse()
+    .filter(unit => unit.undead === 1 && !unit.getFollowing())
+    // .sort((unit1, unit2) => getArmyMight(unit1) - getArmyMight(unit2))
+    // .reverse()
+    .sort((unit1, unit2) => {
+      distanceTo(unit1, findClosestUnblightedHexFromIndex(unit1.hexTarget !== -1 ? unit1.hexTarget : unit1.hexIndex)) -
+      distanceTo(unit2, findClosestUnblightedHexFromIndex(unit2.hexTarget !== -1 ? unit2.hexTarget : unit2.hexIndex))
+    })
 }
 
-
-function delay (delay=1000) {
+function delay(delay=500) {
   return new Promise(resolve => {
     setTimeout(function() {
       return resolve();
@@ -66,7 +69,7 @@ function delay (delay=1000) {
 async function goToHex(unit, hexIndex) {
   game.trigger('select_unit', unit);
   game.trigger('mover_order_auto', hexes[hexIndex]);
-  await delay()
+  await delay(100)
 }
 
 async function nextTurn() {
@@ -116,13 +119,13 @@ function findClosestUnblightedHexFromIndex(hexIndex) {
       if (hexes[currentHex].place && !hexes[currentHex].place.blighted) {
         return currentHex
       } else {
+        const unpassableRivers = hexes[currentHex].rivers.filter(river => !hexes[currentHex].roads.includes(river))
         var validNeighbors = hexes[currentHex].neighbors.filter(hex =>
-          !visited.has(hex) && !hexes[currentHex].rivers.includes(hex)
+          !visited.has(hex) && !unpassableRivers.includes(hex)
         );
         visited.add(currentHex);
         queue = queue.concat(validNeighbors);
       }
-
     }
 
     return null;
@@ -140,23 +143,100 @@ async function buyPlace(place) {
   await delay();
 }
 
+function distanceTo(unit, destinationHex) {
+  let queue = [{ hexIndex: unit.hex.index, distance: 0 }];
+  let visited = new Set();
+  while (queue.length) {
+    let currentNode = queue.shift();
+    let currentHex = currentNode.hexIndex;
+    let distance = currentNode.distance;
+
+    if (currentHex === destinationHex) {
+      return distance;
+    } else {
+      const unpassableRivers = hexes[currentHex].rivers.filter(river => !hexes[currentHex].roads.includes(river))
+      var validNeighbors = hexes[currentHex].neighbors.filter(hex =>
+        !visited.has(hex) && !unpassableRivers.includes(hex)
+      );
+      visited.add(currentHex);
+      queue = queue.concat(validNeighbors.map(neighbor => ({ hexIndex: neighbor, distance: distance + 1 })));
+    }
+  }
+}
+
 async function performTurn() {
   await spendAllValourOnGold();
   await trainAllMilitia();
   await gatherAll(primaryUnit());
   await gatherAll(secondaryUnit());
 
-  var enemyDestinations = findEnemyUnits().map(unit => unit.hexTarget !== -1 ? unit.hexTarget : unit.hexIndex).filter(hex => hex !== -1).map(hex => findClosestUnblightedHexFromIndex(hex));
-  var graveLocations = galaxy.graveList.map(grave => grave.hexIndex);
-  var zombieHexes = [...new Set(enemyDestinations.concat(graveLocations))];
+  var graveLocations = [...new Set(galaxy.graveList.map(grave => grave.hexIndex))];
+  var freeUnits = [...getIndependentUnitsOnBoard()];
+  var enemyDestinations = new Set();
 
-  if (zombieHexes) {
-    for (const [index, unit] of getIndependentUnitsOnBoard().entries()) {
-      await goToHex(unit, zombieHexes[Math.min(zombieHexes.length - 1, index)])
+  var highPriorityGraves = graveLocations.filter(graveLocation => {
+    const unpassableRivers = hexes[graveLocation].rivers.filter(river => !hexes[graveLocation].roads.includes(river))
+    var validNeighbors = hexes[graveLocation].neighbors.filter(hex => !unpassableRivers.includes(hex));
+
+    return validNeighbors.find(neighbor => {
+      return freeUnits.find(unit => unit.hexIndex === neighbor || unit.hexTarget === neighbor)
+    });
+  })
+  var lowPriorityGraves = graveLocations.filter(graveLocation => !highPriorityGraves.includes(graveLocation))
+
+  for (var graveLocation of highPriorityGraves) {
+    if (freeUnits.length) {
+      var chosenUnit = freeUnits.reduce((minUnit, unit) => distanceTo(unit, graveLocation) < distanceTo(minUnit, graveLocation) ? unit : minUnit);
+      freeUnits.splice(freeUnits.indexOf(chosenUnit), 1);
+      await goToHex(chosenUnit, graveLocation)
+    }
+  }
+
+  for (var enemyUnit of findEnemyUnits()) {
+    var availableUnits = freeUnits.filter(unit => getArmyMight(unit) >= getArmyMight(enemyUnit));
+    var availableUnits = availableUnits.length === 0 && freeUnits.length > 0 ? [freeUnits[0]] : availableUnits;
+    var targetHex = enemyUnit.hexTarget !== -1 ? enemyUnit.hexTarget : enemyUnit.hexIndex;
+    if (targetHex !== -1 && availableUnits.length > 0) {
+      var enemyDestination = findClosestUnblightedHexFromIndex(enemyUnit.hexTarget !== -1 ? enemyUnit.hexTarget : enemyUnit.hexIndex);
+
+      if (!enemyDestinations.has(enemyDestination)) {
+        var chosenUnit = availableUnits.reduce((minUnit, unit) => distanceTo(unit, enemyDestination) < distanceTo(minUnit, enemyDestination) ? unit : minUnit);
+
+        freeUnits.splice(freeUnits.indexOf(chosenUnit), 1);
+        enemyDestinations.add(enemyDestination);
+        await goToHex(chosenUnit, enemyDestination)
+      }
+    }
+  }
+
+  for (var graveLocation of lowPriorityGraves) {
+    if (freeUnits.length) {
+      var chosenUnit = freeUnits.reduce((minUnit, unit) => distanceTo(unit, graveLocation) < distanceTo(minUnit, graveLocation) ? unit : minUnit);
+      freeUnits.splice(freeUnits.indexOf(chosenUnit), 1);
+      await goToHex(chosenUnit, graveLocation)
+    }
+  }
+
+  if (graveLocations.length) {
+    for (var unit of freeUnits) {
+      var chosenGraveLocation = graveLocations.reduce((minGraveLocation, graveLocation) => distanceTo(unit, graveLocation) < distanceTo(unit, minGraveLocation) ? minGraveLocation : graveLocation);
+      freeUnits.splice(freeUnits.indexOf(unit), 1);
+      await goToHex(unit, chosenGraveLocation)
+    }
+  }
+
+  if (freeUnits.length) {
+    var enemyUnits = findEnemyUnits();
+    for (var [index, unit] of freeUnits.entries()) {
+      var enemyUnit = enemyUnits[Math.min(enemyUnits.length -1, index)]
+      freeUnits.splice(freeUnits.indexOf(unit), 1);
+      var enemyDestination = findClosestUnblightedHexFromIndex(enemyUnit.hexTarget !== -1 ? enemyUnit.hexTarget : enemyUnit.hexIndex);
+      await goToHex(unit, enemyDestination)
     }
   }
 
   await nextTurn();
+  game.trigger("hide_screen")
   console.log('Turn finished!')
 }
 
@@ -176,6 +256,8 @@ async function init() {
   await deployStrongestUnit();
   await trainAllMilitia();
   await gatherAllUnits();
+  game.trigger("hide_screen")
+
 }
 
 async function main() {
